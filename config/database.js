cfbcabca72ef95d9ba64d41f5daf1ba5b1192091
dversoza <daniel.ve3@gmail.com>
2020-09-20 12:31:30 -0300

    Trying to fix Heroku connection issues

--- config/database.js
+++ config/database.js
@@ -15,7 +15,15 @@ module.exports = ({ env }) => ({
           rejectUnauthorized: false,
         },
       },
-      options: {},
+      options: {
+        pool: {
+          min: 0,
+          max: 10,
+          idleTimeoutMillis: 30000,
+          createTimeoutMillis: 30000,
+          acquireTimeoutMillis: 30000,
+        },
+      },
     },
   },
 });
